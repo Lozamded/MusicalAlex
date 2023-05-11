4cc7162e5df9efda48fba1b1fa2629a06806e0c8
Lozamded <dhiguerafernandez@gmail.com>
2023-05-11 02:24:01 -0400

    ADD: Godoto Project

---
+++ scripts/Camera2D.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Margins/Left.visible = true
+	$Margins/Right.visible = true
+	#pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
